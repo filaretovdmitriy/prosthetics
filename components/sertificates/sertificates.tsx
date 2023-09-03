d9236ab7776dbf240f08a2dd70d76cbb3b26fac2
filaretovdmitriy <filaretovd@yandex.ru>
2023-09-03 22:51:45 +0300

    slider fixed bugs, start to use arrow

--- components/sertificates/sertificates.tsx
+++ components/sertificates/sertificates.tsx
@@ -1,11 +1,24 @@
 "use client";
-import "keen-slider/keen-slider.min.css";
+import { useState, useEffect } from "react";
+import Image from "next/image";
 import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 export default function Sertificates(): JSX.Element {
-  const [keenref] = useKeenSlider<HTMLDivElement>({
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    },
     loop: true,
+    mode: "free",
     slides: {
       perView: 5,
+      spacing: 15,
     },
   });
   return (
@@ -14,67 +27,95 @@ export default function Sertificates(): JSX.Element {
         <h4 className="sertificate__title">
           НАШИ <span>НАГРАДЫ</span> И <span>СЕРТИФИКАТЫ</span>
         </h4>
-        <div className="sertificate__body">
-          <div className="sertificate__arrow-left">
-            <img src="img/sertificate/arrow.svg" alt="" />
+        {loaded && instanceRef.current && <Arrow />}
+
+        <div className="keen-slider" ref={sliderRef}>
+          <div
+            data-fancybox="sertificate"
+            className="keen-slider__slide number-slide1"
+          >
+            <Image
+              src="/img/sertificate/1.png"
+              width={169}
+              height={234}
+              alt=""
+            />
           </div>
-          <div className="sertificate__swiper swiper">
-            <div className="keen-slider" ref={keenref}>
-              <a
-                href="img/sertificate/1.png"
-                data-fancybox="sertificate"
-                className="keen-slider-slide"
-              >
-                <img src="img/sertificate/1.png" alt="" />
-              </a>
-              <a
-                href="img/sertificate/2.png"
-                data-fancybox="sertificate"
-                className="keen-slider-slide"
-              >
-                <img src="img/sertificate/2.png" alt="" />
-              </a>
-              <a
-                href="img/sertificate/3.png"
-                data-fancybox="sertificate"
-                className="keen-slider-slide"
-              >
-                <img src="img/sertificate/3.png" alt="" />
-              </a>
-              <a
-                href="img/sertificate/4.png"
-                data-fancybox="sertificate"
-                className="keen-slider-slide"
-              >
-                <img src="img/sertificate/4.png" alt="" />
-              </a>
-              <a
-                href="img/sertificate/5.png"
-                data-fancybox="sertificate"
-                className="keen-slider-slide"
-              >
-                <img src="img/sertificate/5.png" alt="" />
-              </a>
-              <a
-                href="img/sertificate/3.png"
-                data-fancybox="sertificate"
-                className="keen-slider-slide"
-              >
-                <img src="img/sertificate/3.png" alt="" />
-              </a>
-              <a
-                href="img/sertificate/4.png"
-                data-fancybox="sertificate"
-                className="keen-slider-slide"
-              >
-                <img src="img/sertificate/4.png" alt="" />
-              </a>
-            </div>
+
+          <div
+            data-fancybox="sertificate"
+            className="keen-slider__slide number-slide2"
+          >
+            <Image
+              src="/img/sertificate/2.png"
+              width={169}
+              height={234}
+              alt=""
+            />
           </div>
-          <div className="sertificate__arrow-right">
-            <img src="img/sertificate/arrowRight.svg" alt="" />
+
+          <div
+            data-fancybox="sertificate"
+            className="keen-slider__slide number-slide3"
+          >
+            <Image
+              src="/img/sertificate/3.png"
+              width={169}
+              height={234}
+              alt=""
+            />
+          </div>
+
+          <div
+            data-fancybox="sertificate"
+            className="keen-slider__slide number-slide3"
+          >
+            <Image
+              src="/img/sertificate/3.png"
+              width={169}
+              height={234}
+              alt=""
+            />
+          </div>
+
+          <div
+            data-fancybox="sertificate"
+            className="keen-slider__slide number-slide3"
+          >
+            <Image
+              src="/img/sertificate/3.png"
+              width={169}
+              height={234}
+              alt=""
+            />
+          </div>
+
+          <div
+            data-fancybox="sertificate"
+            className="keen-slider__slide number-slide3"
+          >
+            <Image
+              src="/img/sertificate/3.png"
+              width={169}
+              height={234}
+              alt=""
+            />
           </div>
         </div>
+
+        <div className="sertificate__arrow-right">
+          <img src="img/sertificate/arrowRight.svg" alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Arrow() {
+  return (
+    <>
+      <div className="sertificate__arrow-left">
+        <img src="img/sertificate/arrow.svg" alt="" />
       </div>
     </>
   );
