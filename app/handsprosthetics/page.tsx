68c5b2466a61f50936df218f5008c9dd0a153409
filaretov dmitriy <filaretovd@yandex.ru>
2023-09-06 17:27:16 +0300

    some bug fix, some new bugs

--- app/handsprosthetics/page.tsx
+++ app/handsprosthetics/page.tsx
@@ -1,8 +1,15 @@
 import Innerheader from "@/components/innerheader/innerheader";
 import Innermain from "@/components/innermain/innermain";
-let page = {};
-const getPage = async () => {
-  fetch("http://webapi.a-test.ru/pages", {
+interface PageData {
+  id: number;
+  pagename: string;
+  name: string;
+  link: string;
+  header: string;
+}
+const getPage = async (): Promise<PageData> => {
+  let page = {};
+  fetch("http://webapi.a-test.ru/pages.php", {
     method: "POST",
     headers: { "Content-Type": "application/json;charset=utf-8" },
     body: JSON.stringify({ page: "handsprosthetics" }),
@@ -12,7 +19,9 @@ const getPage = async () => {
       page = data;
     });
 };
-export default function Handsprosthetics() {
+export default async function Handsprosthetics() {
+  let page = await getPage();
+
   return (
     <>
       <Innermain {...page} />
