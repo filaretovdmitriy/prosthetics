7c14628485cd8082846c47c3d5a2604979edc391
Dmitriy Filaretov <info@aisol.ru>
2023-09-08 12:03:40 +0300

    gallery interface

--- interfaces/page.tsx
+++ interfaces/page.tsx
@@ -7,9 +7,21 @@ export interface PageData {
   subtitle: string;
   buttonText: string;
   topPanel: TopPanel;
+  gallery: Gallery;
 }
 
 export interface TopPanel {
   title: string;
   content: string;
+}
+
+export interface Gallery {
+  title: string;
+  content: string;
+  galleryItem: GalleryItem;
+}
+
+export interface GalleryItem {
+  name: string;
+  image: string;
 }
