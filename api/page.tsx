10eb24220673fb6988f20137f625c1d4d2390fad
Dmitriy Filaretov <info@aisol.ru>
2023-09-08 18:59:06 +0300

    start change app structure, it doesn't work still

---
+++ api/page.tsx
@@ -0,0 +1,21 @@
+const getGallery = (page: string) => {
+    const pageGallery = 
+
+
+};
+
+
+
+const getPage = async (): Promise<PageData> => {
+    const data: PageData = await fetch("http://webapi.a-test.ru/page.php", {
+      next: { revalidate: 10 },
+      method: "POST",
+      headers: { "Content-Type": "application/json;charset=utf-8" },
+      body: JSON.stringify({ page: "handsprosthetics" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        return data;
+      });
+    return data;
+  };
