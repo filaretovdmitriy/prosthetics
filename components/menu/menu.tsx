68e0e8d47fbb461ff50163cbc58da45f4e4010b8
filaretov dmitriy <filaretovd@yandex.ru>
2023-09-14 19:36:04 +0300

    remove old code

--- components/menu/menu.tsx
+++ components/menu/menu.tsx
@@ -8,30 +8,11 @@ interface MenuItem {
   link: string;
 }
 
-// const getMenu = async () => {
-//   let resp = await fetch("http://webapi.a-test.ru/menu.php", {
-//     next: { revalidate: 10 },
-//   });
-//   if (resp.ok) {
-//     return resp.json();
-//   } else return [];
-// };
-
 export default function Menu() {
   //   const menuItems = await
   const [loaded, setLoaded] = useState(false);
   const [menuItems, setMenuItems] = useState([]);
   useEffect(() => {
-    // const getMenu = async () => {
-    //   let resp = await fetch("http://webapi.a-test.ru/menu.php", {
-    //     next: { revalidate: 10 },
-    //   });
-    //   if (resp.ok) {
-    //     const loadedMenuItems = await resp.json();
-    //     setMenuItems(loadedMenuItems);
-    //     setLoaded(true);
-    //   } else return [];
-    // };
     const getMenu = () => {};
     fetch("http://webapi.a-test.ru/menu.php", {
       next: { revalidate: 3600 },
