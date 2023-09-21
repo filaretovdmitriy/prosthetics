78510d4e04387b01ef3fb0c86c11c7b9bcb897b3
filaretovdmitriy <filaretovd@yandex.ru>
2023-09-21 23:16:04 +0300

    global context, but it doesn't work

--- components/popup/popup.tsx
+++ components/popup/popup.tsx
@@ -1,25 +1,30 @@
+"use client";
+import { useGlobalContext } from "@/context/store";
 export default function Popup() {
+  const { isPopupOpen, setIsPopupOpen } = useGlobalContext();
   return (
-    <div className="popup" id="popup">
-      <p className="popup__close"></p>
-      <form className="popup__content">
-        <div className="popup__title">
-          БЕСПЛАТНАЯ <br />
-          КОНСУЛЬТАЦИЯ
-        </div>
-        <div className="popup__inputs">
-          <input required type="text" placeholder="Введите ваше имя:" />
-          <input required type="tel" placeholder="Введите ваш телефон:" />
-          <input required type="email" placeholder="Введите вашу почту:" />
-        </div>
-        <div className="popup__bottom">
-          <button className="button">ПРОКОНСУЛЬТИРОВАТЬСЯ</button>
-          <p>
-            Нажимая на кнопку, вы соглашаетесь с условиями{" "}
-            <a href="#">Политики Конфиденциальности</a>
-          </p>
-        </div>
-      </form>
-    </div>
+    isPopupOpen == true && (
+      <div className="popup" id="popup">
+        <p className="popup__close"></p>
+        <form className="popup__content">
+          <div className="popup__title">
+            БЕСПЛАТНАЯ <br />
+            КОНСУЛЬТАЦИЯ
+          </div>
+          <div className="popup__inputs">
+            <input required type="text" placeholder="Введите ваше имя:" />
+            <input required type="tel" placeholder="Введите ваш телефон:" />
+            <input required type="email" placeholder="Введите вашу почту:" />
+          </div>
+          <div className="popup__bottom">
+            <button className="button">ПРОКОНСУЛЬТИРОВАТЬСЯ</button>
+            <p>
+              Нажимая на кнопку, вы соглашаетесь с условиями{" "}
+              <a href="#">Политики Конфиденциальности</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    )
   );
 }
